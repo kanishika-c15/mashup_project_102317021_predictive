9541fa9ca01d7eb8065e51c7c6952e133d8fb6b9
kanishika-c15 <kanishikachhabra@gmail.com>
2026-02-14 00:44:45 +0530

    Refactor Flask app to use environment port

--- app2kan.py
+++ app2kan.py
@@ -55,3 +55,15 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
